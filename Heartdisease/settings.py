21d256e50f7dc4553d54dc8cc6ba1a7bf079d327
Sharadsisodia <sharadsisodia222@gmail.com>
2025-05-01 23:11:02 +0530

    deploy changes

--- Heartdisease/settings.py
+++ Heartdisease/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bbr%zhr-_@!d(4pyxkm0w&9c@_m)0g&(nwjqjz%&6)an-t4(6t'
+# SECRET_KEY = ''
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
