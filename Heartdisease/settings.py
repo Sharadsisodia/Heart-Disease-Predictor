ca5d598641abdfc1e6d0b4b1882090563feb99ce
Sharadsisodia <sharadsisodia222@gmail.com>
2025-05-01 22:56:23 +0530

    settings

--- Heartdisease/settings.py
+++ Heartdisease/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
