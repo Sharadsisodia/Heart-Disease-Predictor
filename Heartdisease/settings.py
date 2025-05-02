8a27846ad66efbb76d09683bea400156e8dfd83b
Sharadsisodia <sharadsisodia222@gmail.com>
2025-05-02 08:12:11 +0530

    settings

--- Heartdisease/settings.py
+++ Heartdisease/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["heart-disease-predictor-z9ss.onrender.com"]
+ALLOWED_HOSTS = [
+    'heart-disease-predictor-z9ss.onrender.com',
+    '127.0.0.1',
+    'localhost'
+]
 # ALLOWED_HOSTS = ['*']
 from decouple import config
 
